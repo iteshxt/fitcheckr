7dc8dfe245087fcac9802ed70d789dc572169093
petrioteer <petrioteer007@gmail.com>
2025-09-28 13:54:29 +0530

    feat: prod build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['localhost'],
+    formats: ['image/webp', 'image/avif'],
+  },
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: false,
+  trailingSlash: false,
 };
 
 
